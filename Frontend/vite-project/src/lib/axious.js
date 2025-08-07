382b9955aebbded1420a5581074416f0d33abafc
unknown <iqrat269@gmail.com>
2025-08-07 23:32:41 +0500

    Update axious.js

--- Frontend/vite-project/src/lib/axious.js
+++ Frontend/vite-project/src/lib/axious.js
@@ -1,7 +1,9 @@
+
 import axios from "axios";
 
-const api=axios.create({
-    baseURL:"https://backend-nu-five-33.vercel.app/"
-})
+const api = axios.create({
+  baseURL: "https://backend-nu-five-33.vercel.app/api", 
+  withCredentials: true,
+});
 
 export default api;
